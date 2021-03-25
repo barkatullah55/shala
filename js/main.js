fe1772b5691fe64d9c2d4f201565b25850c3ebc1
barkatullah55 <67679596+barkatullah55@users.noreply.github.com>
2021-03-25 13:19:52 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 // alert("hi") কোড লিখে টেস্ট করা হয়েছে ওয়েবপেইজে JS কাজ করছে কি না।
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5a59c66dd7591465c706b654/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 
 //Preloader
 $(window).on('load',function(){
